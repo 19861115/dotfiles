c5a84df5c842a98b8b47356d857422a7c4cbc202
seomasanori <ipewlm@hotmail.co.jp>
2014-10-05 18:43:14 +0900

    2014/10/05 (3)

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,9 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim/ ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+git submodule add https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
+git submodule add https://github.com/Shougo/vimproc .vim/bundle/vimproc
+cd .vim/bundle/vimproc
+make
+cd ~/
+mkdir vimbackup
